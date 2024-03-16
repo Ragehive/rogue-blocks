35609130f0071aca9f53f659f71ee4363e416792
destus <destusmod@gmail.com>
2024-03-16 17:20:14 +0100

    added block damage sfx

--- Scenes/block/Block.gd
+++ Scenes/block/Block.gd
@@ -62,6 +62,7 @@ func take_damage(damage):
 	health = max(health - damage, 0)
 	update_healh_label()
 	add_trauma(0.8)
+	$Sfx/DamageSfx.play()
 	if health <= 0:
 		queue_free()
 		Global.score += max_health
